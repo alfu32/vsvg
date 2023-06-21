eaad453c9490f1b6323461e742418b9c2bce99d0
ALFERARU Ioan-Cristian(OIB) <ioan-cristian.alferaru@ec.europa.eu>
2023-06-21 14:05:25 +0200

    refactor(structure)

--- canvas.v
+++ canvas.v
@@ -6,22 +6,23 @@ interface CanvasCompositing {
 }
 
 interface CanvasDrawImage {
-	//drawImage(image CanvasImageSource, dx Number, dy Number)
-	//drawImage(image CanvasImageSource, dx Number, dy Number, dw Number, dh Number)
+	// drawImage(image CanvasImageSource, dx Number, dy Number)
+	// drawImage(image CanvasImageSource, dx Number, dy Number, dw Number, dh Number)
 	draw_image(image CanvasImageSource, sx Number, sy Number, sw Number, sh Number, dx Number, dy Number, dw Number, dh Number)
 }
+
 interface CanvasDrawPath {
-  begin_path()
-  // clip(fillRule CanvasFillRule)
-  clip(path Path2D, fillRule CanvasFillRule)
-  // fill(fillRule CanvasFillRule)
-  fill(path Path2D, fillRule CanvasFillRule)
-  // is_point_in_path(x Number, y Number, fillRule CanvasFillRule) bool
-  is_point_in_path(path Path2D, x Number, y Number, fillRule CanvasFillRule) bool
-  // is_point_in_stroke(x Number, y Number) bool
-  is_point_in_stroke(path Path2D, x Number, y Number) bool
-  stroke()
-  // stroke(path Path2D)
+	begin_path()
+	// clip(fillRule CanvasFillRule)
+	clip(path Path2D, fillRule CanvasFillRule)
+	// fill(fillRule CanvasFillRule)
+	fill(path Path2D, fillRule CanvasFillRule)
+	// is_point_in_path(x Number, y Number, fillRule CanvasFillRule) bool
+	is_point_in_path(path Path2D, x Number, y Number, fillRule CanvasFillRule) bool
+	// is_point_in_stroke(x Number, y Number) bool
+	is_point_in_stroke(path Path2D, x Number, y Number) bool
+	stroke()
+	// stroke(path Path2D)
 }
 
 interface CanvasFillStrokeStyles {
@@ -37,22 +38,24 @@ interface CanvasFilters {
 	filter string
 }
 
-/** An opaque object describing a gradient. It is returned by the methods CanvasRenderingContext2D.createLinearGradient() or CanvasRenderingContext2D.createRadialGradient(). */
+//* An opaque object describing a gradient. It is returned by the methods CanvasRenderingContext2D.createLinearGradient() or CanvasRenderingContext2D.createRadialGradient().
 interface CanvasGradient {
-/**
+	/**
  * Adds a color stop with the given color to the gradient at the given offset. 0.0 is the offset at one end of the gradient, 1.0 is the offset at the other end.
  *
  * Throws an "IndexSizeError" DOMException if the offset is out of range. Throws a "SyntaxError" DOMException if the color cannot be parsed.
- */
-add_color_stop(offset Number, color string)
+	*/
+	add_color_stop(offset Number, color string)
 }
+
 interface CanvasImageData {
 	create_image_data(sw Number, sh Number, settings ImageDataSettings) ImageData
 	// create_image_data(imagedata ImageData) ImageData
 	get_image_data(sx Number, sy Number, sw Number, sh Number, settings ImageDataSettings) ImageData
-	//put_image_data(imagedata ImageData, dx Number, dy Number)
+	// put_image_data(imagedata ImageData, dx Number, dy Number)
 	put_image_data(imagedata ImageData, dx Number, dy Number, dirtyX Number, dirtyY Number, dirtyWidth Number, dirtyHeight Number)
 }
+
 interface CanvasImageSmoothing {
 	image_smoothing_enabled bool
 	image_smoothing_quality ImageSmoothingQuality
@@ -70,6 +73,7 @@ interface CanvasPath {
 	rect(x Number, y Number, w Number, h Number)
 	round_rect(x Number, y Number, w Number, h Number, radii []Number)
 }
+
 interface CanvasPathDrawingStyles {
 	line_cap CanvasLineCap
 	line_dash_offset Number
@@ -80,9 +84,9 @@ interface CanvasPathDrawingStyles {
 	set_line_dash(segments []Number)
 }
 
-/** An opaque object describing a pattern, based on an image, a canvas, or a video, created by the CanvasRenderingContext2D.createPattern() method. */
+//* An opaque object describing a pattern, based on an image, a canvas, or a video, created by the CanvasRenderingContext2D.createPattern() method.
 interface CanvasPattern {
-	/** Sets the transformation matrix that will be used when rendering the pattern during a fill or stroke painting operation. */
+	//* Sets the transformation matrix that will be used when rendering the pattern during a fill or stroke painting operation.
 	set_transform(transform SVGMatrix)
 }
 
@@ -105,26 +109,28 @@ interface CanvasState {
 }
 
 struct TextMetrics {
-/** Returns the measurement described below. */
-actual_bounding_box_ascent  Number
-/** Returns the measurement described below. */
-actual_bounding_box_descent  Number
-/** Returns the measurement described below. */
-actual_bounding_box_left  Number
-/** Returns the measurement described below. */
-actual_bounding_box_right  Number
-/** Returns the measurement described below. */
-font_bounding_box_ascent  Number
-/** Returns the measurement described below. */
-font_bounding_box_descent  Number
-/** Returns the measurement described below. */
-width  Number
+	//* Returns the measurement described below.
+	actual_bounding_box_ascent Number
+	//* Returns the measurement described below.
+	actual_bounding_box_descent Number
+	//* Returns the measurement described below.
+	actual_bounding_box_left Number
+	//* Returns the measurement described below.
+	actual_bounding_box_right Number
+	//* Returns the measurement described below.
+	font_bounding_box_ascent Number
+	//* Returns the measurement described below.
+	font_bounding_box_descent Number
+	//* Returns the measurement described below.
+	width Number
 }
+
 interface CanvasText {
 	fill_text(text string, x Number, y Number, maxWidth Number)
 	measure_text(text string) TextMetrics
 	stroke_text(text string, x Number, y Number, maxWidth Number)
 }
+
 interface CanvasTextDrawingStyles {
 	direction CanvasDirection
 	font string
@@ -134,7 +140,7 @@ interface CanvasTextDrawingStyles {
 }
 
 interface CanvasTransform {
-	get_transform() SVGMatrix// DOMMatrix
+	get_transform() SVGMatrix // DOMMatrix
 	reset_transform()
 	rotate(angle Number)
 	scale(x Number, y Number)
@@ -143,6 +149,7 @@ interface CanvasTransform {
 	transform(a Number, b Number, c Number, d Number, e Number, f Number)
 	translate(x Number, y Number)
 }
+
 interface CanvasRenderingContext2DSettings {
 	alpha bool
 	color_space PredefinedColorSpace
@@ -150,8 +157,8 @@ interface CanvasRenderingContext2DSettings {
 	will_read_frequently bool
 }
 
-/** The CanvasRenderingContext2D interface, part of the Canvas API, provides the 2D rendering context for the drawing surface of a <canvas> element. It is used for drawing shapes, text, images, and other objects. */
-interface CanvasRenderingContext2D{
+//* The CanvasRenderingContext2D interface, part of the Canvas API, provides the 2D rendering context for the drawing surface of a <canvas> element. It is used for drawing shapes, text, images, and other objects.
+interface CanvasRenderingContext2D {
 	CanvasCompositing
 	CanvasDrawImage
 	CanvasDrawPath
